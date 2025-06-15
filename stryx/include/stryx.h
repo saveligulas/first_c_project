7812aff00cf12e5bebd5fbf1f95fcc2ab01124dc
Saveli Gulas <saveli.gulas@gmail.com>
2025-06-15 21:28:37 +0200

    renamed stringlib to stryx

--- stryx/include/stryx.h
+++ stryx/include/stryx.h
@@ -1,13 +1,12 @@
 #ifndef STRINGLIB_H
 #define STRINGLIB_H
 
-#include <stddef.h>
 #include <stdbool.h>
 
 typedef struct String String;
 
 String* string_create_empty();
-String* string_create(const char* initial);
+String *string_create(const char* initial);
 String *string_create_explicit(const int capacity);
 void    string_destroy(String* str);
 const char* string_cstr(const String* str);
@@ -16,13 +15,13 @@ void string_print(const String *str);
 
 bool    string_append(String* str, const char* text);
 bool    string_append_char(String* str, char c);
-String* string_clone(const String* str);
+String *string_clone(const String* str);
 void    string_clear(String** str);
 
 bool    string_insert(String* str, size_t pos, const char* text);
 size_t     string_find(const String* str, const char* substr);
 bool    string_equals(const String* a, const String* b);
-String* string_substring(const String* str, size_t start, size_t len);
+String *string_substring(const String* str, size_t start, size_t len);
 
 void    string_reverse(String* str);
 void    string_to_upper(String* str);
