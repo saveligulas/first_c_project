0ad4c500f0340d7cf7c85b2aa0b2d98783dfaff8
Saveli Gulas <saveli.gulas@gmail.com>
2025-06-15 21:30:15 +0200

    import fix and implemented string_reverse and string_replace_char

--- stryx/src/string_manipulation.c
+++ stryx/src/string_manipulation.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "../include/stringlib.h"
-#include "../internal/string_internal.h"
+#include "../include/stryx.h"
+#include "../internal/_string_internal.h"
 
 bool string_append(String* str, const char* text) {
    if (_is_invalid(str)) return false;
@@ -46,4 +46,28 @@ bool string_insert(String* str, size_t pos, const char* text) {
    str->length += len;
    str->data[str->length] = NULL_CHAR;
    return true;
+}
+
+void string_reverse(String *str) {
+   if (_is_invalid(str)) return;
+
+   for (size_t i = 0; i < str->length / 2; i++) {
+      const char tmp = str->data[i];
+      const size_t inverted_i = str->length - i - 1;
+      str->data[i] = str->data[inverted_i];
+      str->data[inverted_i] = tmp;
+   }
+}
+
+void string_to_upper(String* str) {
+
+}
+void string_replace_char(String* str, char old_char, char new_char) {
+   if (_is_invalid(str)) return;
+
+   for (size_t i = 0; i < str->length; i++) {
+      if (str->data[i] == old_char) {
+         str->data[i] = new_char;
+      }
+   }
 }
